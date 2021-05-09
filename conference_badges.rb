ccea7be17c1254ec1d25d96731ae4a299e05267c
Yoshi Mejia <yoshimejia00@gmail.com>
2021-05-09 15:40:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  arr = [ ]
+  names.each {|i| arr << "Hello, my name is #{i}."}
+  arr
+end
+
+def assign_rooms(speaker)
+#  arr = [ ]
+  speaker.each do |name, index|
+    puts "Hello, #{name}! You'll be assigned to room #{index}!"
+end
+end
